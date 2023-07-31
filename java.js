b1a95251ca04ab7ca9f04590d919fadef347861f
masoodurre <masoodurehmaan046@.com>
2023-07-31 12:17:51 -0700

    new

---
+++ java.js
@@ -0,0 +1,39 @@
+
+let two = document.getElementById('three')
+let fou = document.getElementById('four')
+// let fams = document.getElementById('five')
+
+
+function number1() {
+ two.innerHTML= '';
+
+  const number3= document.getElementById('one').value
+
+    for ( let number2=1; number2<11; number2++)
+  two.innerHTML+= `${number3}x ${number2}=${number2*number3}</br> `
+
+}
+
+function mum() {
+  
+  fou.innerHTML= ''
+   const number3= document.getElementById('on').value
+ 
+     for ( let number2=1; number2<11; number2++)
+   fou.innerHTML+= `${number3}x ${number2}=${number2*number3}</br> `
+ 
+ }
+//  This is and
+
+
+//  function threet() {
+  
+//   fams.innerHTML= ''
+//    const number4= document.getElementById('first').value
+//    const number5= document.getElementById('second').value
+//    const number6= document.getElementById('add').value
+ 
+//      for ( let number2=1; number2<11; number2++)
+//    fams.innerHTML+= `${number3}x ${number2}=${number2*number3}</br> `
+ 
+//  }
