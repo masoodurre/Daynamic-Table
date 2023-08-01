7d84250b798fbacc69b01fcc7de1987b256815fb
masoodurre <masoodurehmaan046@.com>
2023-08-01 08:59:28 -0700

    will

--- java.js
+++ java.js
@@ -1,39 +1,42 @@
 
-let two = document.getElementById('three')
-let fou = document.getElementById('four')
-// let fams = document.getElementById('five')
+// let two = document.getElementById('three')
+// let fou = document.getElementById('four')
+let fams = document.getElementById('five')
 
 
-function number1() {
- two.innerHTML= '';
+// function number1() {
+//  two.innerHTML= '';
 
-  const number3= document.getElementById('one').value
+//   const number3= document.getElementById('one').value
 
-    for ( let number2=1; number2<11; number2++)
-  two.innerHTML+= `${number3}x ${number2}=${number2*number3}</br> `
+//     for ( let number2=1; number2<11; number2++)
+//   two.innerHTML+= `${number3}x ${number2}=${number2*number3}</br> `
 
-}
+// }
 
-function mum() {
+// function mum() {
   
-  fou.innerHTML= ''
-   const number3= document.getElementById('on').value
+//   fou.innerHTML= ''
+//    const number3= document.getElementById('on').value
  
-     for ( let number2=1; number2<11; number2++)
-   fou.innerHTML+= `${number3}x ${number2}=${number2*number3}</br> `
+//      for ( let number2=1; number2<11; number2++)
+//    fou.innerHTML+= `${number3}x ${number2}=${number2*number3}</br> `
  
- }
+ 
 //  This is and
 
 
-//  function threet() {
+ function threet() {
   
-//   fams.innerHTML= ''
-//    const number4= document.getElementById('first').value
-//    const number5= document.getElementById('second').value
-//    const number6= document.getElementById('add').value
- 
-//      for ( let number2=1; number2<11; number2++)
-//    fams.innerHTML+= `${number3}x ${number2}=${number2*number3}</br> `
- 
-//  }
+   const number4= document.getElementById('start').value
+   const number5= document.getElementById('and').value
+   const number6= document.getElementById('enter').value
+   
+  fams.innerHTML= ''
+     for ( let index=number4; index<=number5; index++){
+   fams.innerHTML+= number6+ " x " + index+ " = " + index*number6+"</br>"
+     }
+ }
+
+
+
